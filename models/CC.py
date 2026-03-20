fc177d24bac5b824e7cca321ab570691cb1411e6
touficabiyounes <toufic.abi-younes@etu.u-paris.fr>
2026-03-20 11:34:51 +0100

    test : CSRnet laplace

--- models/CC.py
+++ models/CC.py
@@ -40,6 +40,7 @@ class CrowdCounter(nn.Module):
         output = self.CCN(img)
 
         if isinstance(output, tuple):
+            print('############################### use uncertainty loss ###############################################')
             mu, b = output
             if cfg.LOSS_TYPE == 'laplace':
                 self.loss_mse = self.build_laplace_loss(mu.squeeze(), b.squeeze(), gt_map.squeeze())
