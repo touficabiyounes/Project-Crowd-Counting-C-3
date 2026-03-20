14711ee939f36d0825454a864fcb75b927b60edd
touficabiyounes <toufic.abi-younes@etu.u-paris.fr>
2026-03-20 11:37:36 +0100

    test : CSRnet laplace

--- models/SCC_Model/VGG.py
+++ models/SCC_Model/VGG.py
@@ -1,5 +1,4 @@
 from config import cfg
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from misc.layer import Conv2d, FC
@@ -12,21 +11,25 @@ class VGG(nn.Module):
     def __init__(self, pretrained=True):
         super(VGG, self).__init__()
         vgg = models.vgg16(pretrained=pretrained)
-        # if pretrained:
-        #     vgg.load_state_dict(torch.load(model_path))
         features = list(vgg.features.children())
         self.features4 = nn.Sequential(*features[0:23])
-
+        self.use_uncertainty = cfg.LOSS_TYPE in ['gaussian', 'laplace']
 
         self.de_pred = nn.Sequential(Conv2d(512, 128, 1, same_padding=True, NL='relu'),
                                      Conv2d(128, 1, 1, same_padding=True, NL='relu'))
 
-
+        if self.use_uncertainty:
+            self.log_var = nn.Sequential(Conv2d(512, 128, 1, same_padding=True, NL='relu'),
+                                         Conv2d(128, 1, 1, same_padding=True, NL=None))
 
     def forward(self, x):
-        x = self.features4(x)       
-        x = self.de_pred(x)
+        x = self.features4(x)
+        mu = F.softplus(self.de_pred(x))
+        mu = F.interpolate(mu, scale_factor=8, mode='bilinear', align_corners=False)
 
-        x = F.interpolate(x,scale_factor=8)
-
-        return x
+        if not self.use_uncertainty:
+            return mu
+        
+        b = F.softplus(self.log_var(x)) + 1e-3
+        b = F.interpolate(b, scale_factor=8, mode='bilinear', align_corners=False)
+        return mu, b
